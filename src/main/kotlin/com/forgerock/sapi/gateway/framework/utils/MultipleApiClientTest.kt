829a96a05ec7f9fa2badac39604e6ff0404e5076
Dan Badham <dan.badham@forgerock.com>
2024-05-03 13:30:19 +0100

    Add negative tests for accessing the plain FAPI API
    Rename ExampleTest to PlainFapiApiEndpointTest.
    
    Add tests which verify that a client cannot access the API with an invalid access_token, including the case where client2 tries to use a valid access_token created by client1.

--- src/main/kotlin/com/forgerock/sapi/gateway/framework/utils/MultipleApiClientTest.kt
+++ src/main/kotlin/com/forgerock/sapi/gateway/framework/utils/MultipleApiClientTest.kt
@@ -1,13 +1,23 @@
 package com.forgerock.sapi.gateway.framework.utils
 
+import assertk.assertThat
+import assertk.assertions.isGreaterThanOrEqualTo
 import com.forgerock.sapi.gateway.framework.apiclient.ApiClient
 import com.forgerock.sapi.gateway.framework.configuration.ConfigurationManager
+import org.junit.jupiter.api.BeforeAll
 
 abstract class MultipleApiClientTest {
+
     companion object {
         @JvmStatic
         fun getApiClients(): List<ApiClient> {
             return ConfigurationManager.getApiClients()
+        }
+
+        @JvmStatic
+        @BeforeAll
+        fun validateConfig() {
+            assertThat(getApiClients().size).isGreaterThanOrEqualTo(2)
         }
     }
 }
---
+++ src/test/kotlin/com/forgerock/sapi/gateway/core/PlainFapiApiEndpointTest.kt
@@ -0,0 +1,124 @@
+package com.forgerock.sapi.gateway.core
+
+import assertk.assertThat
+import assertk.assertions.contains
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.isTrue
+import com.forgerock.sapi.gateway.common.constants.OAuth2AuthorizeRequestJwtClaims
+import com.forgerock.sapi.gateway.common.constants.OAuth2Constants
+import com.forgerock.sapi.gateway.framework.apiclient.ApiClient
+import com.forgerock.sapi.gateway.framework.configuration.ConfigurationManager.Loader.apiUnderTest
+import com.forgerock.sapi.gateway.framework.data.AccessToken
+import com.forgerock.sapi.gateway.framework.data.RequestParameters
+import com.forgerock.sapi.gateway.framework.utils.GsonUtils
+import com.forgerock.sapi.gateway.framework.utils.MultipleApiClientTest
+import com.forgerock.sapi.gateway.ob.uk.framework.constants.PLAIN_FAPI_ACR_CLAIM
+import com.github.kittinunf.fuel.core.Headers
+import com.github.kittinunf.fuel.core.isSuccessful
+import com.nimbusds.jose.util.JSONObjectUtils
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.UUID
+
+/**
+ * Tests for the SAPI-G core Plain FAPI API endpoint (/rs/fapi/api).
+ */
+class PlainFapiApiEndpointTest : MultipleApiClientTest() {
+
+    private val plainFapiEndpointUrl = apiUnderTest.getEndpointUrl("plainFapiEndpoint")
+
+    @BeforeEach
+    fun beforeEach() {
+    }
+
+    @ParameterizedTest
+    @MethodSource("getApiClients")
+    fun successApiClientsDoPlainFapi(apiClient: ApiClient) {
+        // Given
+        val registrationResponse = apiClient.doDynamicClientRegistration(apiUnderTest)
+        assertThat(registrationResponse).isNotNull()
+
+        val accessToken = getAccessToken(apiClient)
+
+        // When
+        val (_, response, result) = apiClient.fuelManager.get(plainFapiEndpointUrl)
+            .header(Headers.AUTHORIZATION, "Bearer ${accessToken.access_token}").responseString()
+        assertThat(response.isSuccessful).isTrue()
+        assertThat(result.get()).contains("user")
+    }
+
+    private fun getAccessToken(
+        apiClient: ApiClient
+    ): AccessToken {
+        val scopes = "openid accounts"
+        val resourceOwner = apiUnderTest.resourceOwners[0]
+        val responseTypes = "code id_token"
+        val additionalClaims: List<Pair<String, Any>> = getPlainFapiClaims()
+        val accessToken = apiUnderTest.oauth2Server.getAuthorizationCodeAccessToken(
+            apiClient,
+            apiUnderTest,
+            scopes,
+            resourceOwner,
+            responseTypes,
+            additionalClaims
+        )
+        return accessToken
+    }
+
+    @Test
+    fun failsToAccessProtectedEndpointWhenNoAccessTokenProvided() {
+        val apiClient = getApiClients().first()
+
+        val (_, response, _) = apiClient.fuelManager.get(plainFapiEndpointUrl).responseString()
+        assertThat(response.statusCode).isEqualTo(401)
+    }
+
+    @Test
+    fun failsToAccessProtectedEndpointWhenInvalidAccessTokenProvided() {
+        val apiClient = getApiClients().first()
+
+        val (_, response, _) = apiClient.fuelManager.get(plainFapiEndpointUrl)
+            .header("Authorization", "Bearer invalid")
+            .responseString()
+
+        assertThat(response.statusCode).isEqualTo(401)
+    }
+
+    @Test
+    fun failsWhenDifferentClientTriesToUseAccessToken() {
+        val client1 = getApiClients()[0]
+        client1.doDynamicClientRegistration(apiUnderTest)
+        val client2 = getApiClients()[1]
+        client2.doDynamicClientRegistration(apiUnderTest)
+
+        val accessTokenForClient1 = getAccessToken(client1)
+
+        // Validate that client1 can access the resource
+        val (_, response1, result1) = client1.fuelManager.get(plainFapiEndpointUrl)
+            .header(Headers.AUTHORIZATION, "Bearer ${accessTokenForClient1.access_token}").responseString()
+        assertThat(response1.isSuccessful).isTrue()
+        assertThat(result1.get()).contains("user")
+
+        // Validate that client2 cannot access the resource using client1's token
+        val (_, response2, _) = client2.fuelManager.get(plainFapiEndpointUrl)
+            .header(Headers.AUTHORIZATION, "Bearer ${accessTokenForClient1.access_token}").responseString()
+        assertThat(response2.statusCode).isEqualTo(401)
+    }
+
+    private fun getPlainFapiClaims(): List<Pair<String, Any>> {
+        val idToken = RequestParameters.Claims.IdToken(
+            RequestParameters.Claims.IdToken.Acr(true, PLAIN_FAPI_ACR_CLAIM), null
+        )
+
+        val claims = RequestParameters.Claims(idToken, null)
+        val jsonClaims = JSONObjectUtils.parse(GsonUtils.gson.toJson(claims))
+        val claimPair = Pair(OAuth2AuthorizeRequestJwtClaims.CLAIMS, jsonClaims)
+        val nonce = Pair(OAuth2Constants.NONCE, UUID.randomUUID().toString())
+        val state = Pair(OAuth2Constants.STATE, UUID.randomUUID().toString())
+        return listOf(claimPair, nonce, state)
+    }
+
+}
