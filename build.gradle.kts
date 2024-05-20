efd32194438010fe15a267f2268db44c9022f8fd
shaunharrisonFR <115990205+shaunharrisonFR@users.noreply.github.com>
2024-05-20 14:52:38 +0100

    1404: Update version and remove unwanted release var (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
  */
 // project version
 // pom artifact version used when the built artifact is published
-version = "2.2.0"
-// Test jar library version used in the task 'generateTestJar'
-val release = "2.2.0"
+version = "3.0.0"
 
 val junitVersion = "5.10.2"
 val fuelVersion = "2.3.1"
@@ -111,7 +109,7 @@ tasks.register<Jar>("generateTestJar") {
     group = "specific"
     description = "Generate a non-executable jar library tests"
     archiveClassifier.set("tests")
-    archiveFileName.set("${project.name}-${project.version}-$release.jar")
+    archiveFileName.set("${project.name}-${project.version}-$version.jar")
     from(sourceSets.test.get().allSource)
     from(sourceSets.main.get().allSource)
     dependsOn("testClasses")
